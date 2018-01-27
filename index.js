904ec1db87117bc7576b7954c860420d13f4e02f
Zeynep Öner <26029554@arcelik.com>
2018-01-27 21:51:41 +0300

    html is modified

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ app.get('/', function(req, res) {
 });
 
 app.get('/views/redirect.html' , function(req,res) {
-    //res.redirect('https://' + req.get('host') + req.url)
-    res.redirect(__dirname + '/redirect.html');    
+    res.sendfile(__dirname + '/views/redirect.html')
 });
 
 
