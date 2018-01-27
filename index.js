3a5285b725c67e3ef17551f2e1c25df39389a245
Zeynep Öner <26029554@arcelik.com>
2018-01-27 14:24:56 +0300

    package.json is modified

--- index.js
+++ index.js
@@ -5,20 +5,21 @@ var express = require('express'),
     errorHandler = require('express-error-handler'),
     app = express();
 
-var logFmt = require("logfmt");    
+var logFmt = require("logfmt");
+
 app.set('views', __dirname + '/views') ;
-    
-   
+
+
 app.get('/' , function(req,res) {
     res.sendfile('views/index.html');
-});
-   
+} );
 
 
-app.set('port', process.env.PORT || 3001);
+
+app.set('port', process.env.PORT || 1337);
+
 app.use(express.static(__dirname + '/client')); 
 app.use(errorHandler());
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
-
