b4fed83b7ea6f2c5dfa676e97b3a15b4092fbfb9
Zeynep Öner <26029554@arcelik.com>
2018-01-31 21:09:32 +0300

    postman

--- index.js
+++ index.js
@@ -10,32 +10,13 @@ var logFmt = require("logfmt");
 //app.set('views', __dirname + '/views') ;
 
 app.get('/', function(req, res) {
-
     res.sendfile(__dirname + '/views/index.html');        
-     
-    var options = { method: 'POST',
-    url: 'https://yetkiliservis-test.arcelik.com/wsaftersales/ServicePaperService.svc/ProductOrderOperationService',
-    headers: 
-       { 'Postman-Token': 'c230d62e-929c-ea04-b8bd-0bb04af28c12',
-         'Cache-Control': 'no-cache',
-         SessionToken: 'Guld',
-         'Content-Type': 'application/x-www-form-urlencoded' } };
-    
-    request(options, function (error, response, body) {
-      if (error) throw new Error(error);
-    
-      alert(body);
 });
 
 app.get('/workorder' , function(req,res) {
     res.sendfile('views/redirect.html', {root: __dirname });   
 });
 
-
-
-
-
-});
 
 
 app.set('port', process.env.PORT || 1337);
