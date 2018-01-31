b16a86e511c5ca4e9f0ce7691899a92f6c65565d
Zeynep Öner <26029554@arcelik.com>
2018-01-31 22:57:54 +0300

    postman

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ json: true };
 
 request(options, function (error, response, body) {
     if (error) throw new Error(error);
-    res.send(response);
+    res.send(body.response);
 });
     
         
