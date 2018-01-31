6f2e2adb640d2457e114ec4f68494638a7a66857
Zeynep Öner <26029554@arcelik.com>
2018-01-31 21:31:17 +0300

    postman

--- client/lib/script.js
+++ client/lib/script.js
@@ -181,7 +181,7 @@ var data = JSON.stringify({
 
 
           xhr.setRequestHeader("Access-Control-Allow-Origin", "*");          
-          xhr.setRequestHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Cache-Control,Content-Encoding ,Date, SessionToken, Cache-Control, Postman-Token, Origin,Accept, X-Requested-With, Content-Type,Server,Set-Cookie,Transfer-Encoding,X-AspNet-Version ,X-IP,X-Powered-By, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin");       
+          xhr.setRequestHeader("Access-Control-Allow-Headers", "*");       
           //xhr.setRequestHeader("Access-Control-Allow-Methods", "POST");
           
           xhr.withCredentials = false;
