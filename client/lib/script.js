52ba16d92d8d851cdac13bfaa5846ee1f622ec30
Zeynep Öner <26029554@arcelik.com>
2018-01-31 21:27:47 +0300

    postman

--- client/lib/script.js
+++ client/lib/script.js
@@ -100,7 +100,69 @@ testApp.controller('testController' , ['$scope','$http','$window', function ($sc
 
 */
 
-          var data = null;
+var data = JSON.stringify({
+    "ProductOrderOperationRequest": {
+      "MainSourceApplicationProcces": "ECOM Uygulama İsimi",
+      "SourceApplication": "DMS Uygulama İsmi",
+      "MainSourceOrderProccesId": "Ecom1 Id Olacak",
+      "SourceOrderId": "DMS1 Id Olacak",
+      "MainSourceProccesStatus": "Ecom Approve",
+      "SourceStatus": "DMS Approve",
+      "AsistDealerCode": "21534701",
+      "DealerCode": "21534701",
+      "AsistBranchDealerCode": "0",
+      "Tckn": "18623292306",
+      "TaxOffice": "İSTANBUL-KARTAL",
+      "TaxId": "1234567890",
+      "Email": "ozkan.lostar@lostarltd.com.tr",
+      "Note": "Ana order kaydına girilen note",
+      "Name": "ÖZKAN",
+      "Surname": "LOSTAR",
+      "Phone1": "5323138414",
+      "Phone2": "2163950981",
+      "Phone3": "5323138414",
+      "Address": "CAMI MAH. PAPAZ ÇEŞME CAD. MEHTER SOK.   NO: 4 81700",
+      "Neighborhood": "CAMI",
+      "District": "TUZLA",
+      "City": "İSTANBUL",
+      "Urgent": "0",
+      "ContactPerson": "Özkan LOSTAR",
+      "ContactPhone": "5333836529",
+      "PreferredServiceShop": "0",
+      "DeliveryDate": "26/1/2018",
+      "ExternalOrderId": "Önceden oluşturulan Order No varsa ve durum update geliyorsa yazılır.",
+      "ProductOrderOperationDetailRequest": [
+        {
+          "MainSourceOrderProcessId": "Ecom1 Id Olacak",
+          "SourceOrderId": "DMS1 Id Olacak",
+          "MainSourceOrderProcessStatus": "ECOM Approve",
+          "ConsignmentId": "00002345-c-1",
+          "ProductCode": "VZT000",
+          "ProductBarcode": "VZT000150087191201",
+          "MaliId": "AS0000216212",
+          "DetailNote": "Montaj Yapabilirsiniz",
+          "WareHouseCode": "01",
+          "WareHouseType": "Bayi Depo",
+          "WareHouseAddress": "CAMİ MAH.PAPAZÇEŞME CAD.NO:21 TUZLA",
+          "WareHouseNeighborhood": "CAMI",
+          "WareHouseDistrict": "TUZLA",
+          "WareHouseCity": "İSTANBUL",
+          "Product": "9103 YP  9 KG 1000 DD TEK SU BEYAZ ÇAM MAK",
+          "OperationType": "Montaj",
+          "ProductReturnCheck": "0",
+          "ExtraWarrantyType": "0",
+          "ProductExposeCheck": "0",
+          "SourceOrderStatus": "DMS Approve",
+          "ParoId": "17051509295500055025",
+          "CrmKey": "5425928388",
+          "InvoiceNr": "007557",
+          "InvoiceDate": "2017-04-27 00:00:00",
+          "NaceId": "471101",
+          "SectorId": "01"
+        }
+      ]
+    }
+  });
           
           var xhr = new XMLHttpRequest();
           //xhr.withCredentials = true;
@@ -112,15 +174,17 @@ testApp.controller('testController' , ['$scope','$http','$window', function ($sc
           });
           
           xhr.open("POST", "https://yetkiliservis-test.arcelik.com/wsaftersales/ServicePaperService.svc/ProductOrderOperationService");
-          xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+          xhr.setRequestHeader("Content-Type", "application/json");
           xhr.setRequestHeader("SessionToken", "12345678-1234-1234-1234-123456789101");
           xhr.setRequestHeader("Cache-Control", "no-cache");
-          xhr.setRequestHeader("Postman-Token", "62c7c72a-6088-d796-6495-c41e526c85aa");
+          xhr.setRequestHeader("Postman-Token", "228cda55-1f70-8fb0-4fdc-5994783f19a2");
+
+
           xhr.setRequestHeader("Access-Control-Allow-Origin", "*");          
           xhr.setRequestHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Cache-Control,Content-Encoding ,Date, SessionToken, Cache-Control, Postman-Token, Origin,Accept, X-Requested-With, Content-Type,Server,Set-Cookie,Transfer-Encoding,X-AspNet-Version ,X-IP,X-Powered-By, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin");       
           xhr.setRequestHeader("Access-Control-Allow-Methods", "POST");
           
-          xhr.withCredentials = false;
+          xhr.withCredentials = true;
           
           
           xhr.send(data);
