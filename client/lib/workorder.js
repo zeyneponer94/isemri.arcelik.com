2da70f371b4b30489ae9234d4fab9b6b84225ec1
Zeynep Öner <26029554@arcelik.com>
2018-02-02 22:30:08 +0300

    postman

--- client/lib/workorder.js
+++ client/lib/workorder.js
@@ -87,13 +87,13 @@
             method: 'POST',
             url: 'http://yetkiliservis-test.arcelik.com/wsaftersales/ServicePaperService.svc/ProductOrderOperationService',
             headers: {
-              SessionToken: "4737B2FC-DE66-4741-B7A8-07646813D890",
+              "SessionToken": "4737B2FC-DE66-4741-B7A8-07646813D890",
             /*  "Cache-Control": "no-cache",
               'Access-Control-Allow-Origin':'*',
               'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE',
               'Access-Control-Allow-Headers':'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Access-Control-Request-Method, Access-Control-Request-Headers,Content-Type,Access-Control-Allow-Origin,servicetype, Cache-Control, SessionToken, **Authorization** ',
               'Access-Control-Allow-Credentials':false,*/
-              servicetype: "INTHEBOX1",
+              "servicetype": "INTHEBOX1",
               "Content-Type": "application/json"
             },
             data: {
