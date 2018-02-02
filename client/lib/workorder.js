e8a02029e1b0605f84fa27c7db63886c4304f72d
Zeynep Öner <26029554@arcelik.com>
2018-02-02 14:51:00 +0300

    postman

--- client/lib/workorder.js
+++ client/lib/workorder.js
@@ -83,93 +83,14 @@
         $scope.createWorkOrder = function () 
         {        
 
-          var data = JSON.stringify([
-            {
-              "PK": "",
-              "MainSourceApplicationProcces": "DMS",
-              "SourceApplication": "DMS",
-              "MainSourceOrderProccesId": "DMS1",
-              "SourceOrderId": "DMS1",
-              "MainSourceProccesStatus": "Approve",
-              "SourceStatus": "Approve",
-              "DealerCode": "342122",
-              "AsistDealerCode": "342122",
-              "AsistBranchDealerCode": "342122",
-              "Note": "10",
-              "Name": "Serdar",
-              "Surname": "Uysal",
-              "Phone1": "5373579059",
-              "Phone2": "2163964187",
-              "Phone3": "",
-              "Email": "serdar.uysal@arcelik.com",
-              "TaxOffice": "",
-              "TaxId": "",
-              "Tckn": "",
-              "Address": "Kavakpınar Mah Apdi İpekçi Cad No:5",
-              "Neighborhood": "KAVAKPINAR",
-              "District": "PENDİK",
-              "City": "İSTANBUL",
-              "Urgent": "0",
-              "ContactPerson": "Deneme",
-              "ContactPhone": "2167865438",
-              "PreferredServiceShop": "NULL",
-              "DeliveryDate": "13.02.2018 08:54:00",
-              "ExternalOrderId": "",
-              "InvoiceAcceptPhone": "5373579059",
-              "InvoiceAcceptName": "Serdar",
-              "InvoiceAcceptSurname": "Uysal",
-              "ProductOrderDetail": [
-                {
-                  "ConsignmentId": "1",
-                  "MainSourceOrderProcessId": "DMS1",
-                  "SourceOrderId": "DMS1",
-                  "PK": "",
-                  "R_Counter": "",
-                  "SS_R_Counter": "",
-                  "MainSourceOrderProcessStatus": "Approve",
-                  "WareHouseCode": "12457",
-                  "WareHouseType": "1",
-                  "WareHouseAddress": "Depo Adresi",
-                  "WareHouseNeighborhood": "BATI",
-                  "WareHouseDistrict": "PENDİK",
-                  "WareHouseCity": "İSTANBUL",
-                  "ProductCode": "6211101000",
-                  "Product": "ARY-5500 E ÇAMAŞIR MAK.(Y-326) ÇİFT",
-                  "OperationType": "Montaj",
-                  "ProductReturnCheck": "0",
-                  "ExtraWarrantyType": "1",
-                  "ProductExposeCheck": "0",
-                  "SourceOrderStatus": "Approve",
-                  "ProductBarcode": "",
-                  "DetailNote": "Test satır 1",
-                  "ParoId": "",
-                  "InvoiceNr": "AAFF111SFFEWQ",
-                  "InvoiceDate": "13.02.2018 08:54:00",
-                  "MaliId": "",
-                  "NaceId": "",
-                  "SectorId": "",
-                  "CrmKey": ""
-                }
-              ]
-            }
-          ]);
-          
-          var xhr = new XMLHttpRequest();
-          xhr.withCredentials = true;
-          
-          xhr.addEventListener("readystatechange", function () {
-            if (this.readyState === 4) {
-              console.log(this.responseText);
-            }
+          $http({method: 'GET', url: '/postman'}).
+          success(function(data, status) {  
+              var url = "https://safe-inlet-65958.herokuapp.com/postman";
+              $window.location = url;
+          }).
+          error(function(data, status) {
+              $scope.dataset = data || "Request failed "; 
           });
-          
-          xhr.open("POST", "http://yetkiliservis-test/wsaftersales/ServicePaperService.svc/ProductOrderOperationService");
-          xhr.setRequestHeader("SessionToken", "4737B2FC-DE66-4741-B7A8-07646813D890");
-          xhr.setRequestHeader("Cache-Control", "no-cache");
-          xhr.setRequestHeader("Postman-Token", "342a2ba6-e097-e303-2404-5fa0d997d6e1");
-          xhr.setRequestHeader("servicetype", "INTHEBOX1");
-          xhr.setRequestHeader("Content-Type", "application/json");
-          xhr.send(data);
         } 
 
 
