ada1669cd0656ce0ae0357a98cdc17134adbf94e
Zeynep Öner <26029554@arcelik.com>
2018-02-03 16:28:22 +0300

    postman

--- client/lib/workorder.js
+++ client/lib/workorder.js
@@ -98,7 +98,7 @@
           $http({
             method: "GET", 
             url: 'https://thworkorderfapp.azurewebsites.net/api/citytype',
-            params: {prorvince:$scope.singleSelect}          
+            params: {province:$scope.provinceSelect}          
           }) 
           .then(function(response){ 
               $scope.city = [];                    
@@ -111,112 +111,29 @@
           });          
         }
 
-        $scope.create = function () 
+        $scope.create_workorder = function () 
         {        
-
-          var req = {
-            method: 'POST',
-            url: 'http://yetkiliservis-test.arcelik.com/wsaftersales/ServicePaperService.svc/ProductOrderOperationService',
-            headers: {
-              "SessionToken": "4737B2FC-DE66-4741-B7A8-07646813D890",
-              'Access-Control-Allow-Headers':'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Access-Control-Request-Method, Access-Control-Request-Headers,Content-Type,Access-Control-Allow-Origin,servicetype, Cache-Control, SessionToken, Authorization ',
-              
-            /*  "Cache-Control": "no-cache",
-              'Access-Control-Allow-Origin':'*',
-              'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE',
-              'Access-Control-Allow-Credentials':false,*/
-              "servicetype": "INTHEBOX1",
-              "Content-Type": "application/json"
-            },
-            data: {
-              "PK": "",
-              "MainSourceApplicationProcces": "DMS",
-              "SourceApplication": "DMS",
-              "MainSourceOrderProccesId": "DMS1",
-              "SourceOrderId": "DMS1",
-              "MainSourceProccesStatus": "Approve",
-              "SourceStatus": "Approve",
-              "DealerCode": "342122",
-              "AsistDealerCode": "342122",
-              "AsistBranchDealerCode": "342122",
-              "Note": "10",
-              "Name": "Serdar",
-              "Surname": "Uysal",
-              "Phone1": "5373579059",
-              "Phone2": "2163964187",
-              "Phone3": "",
-              "Email": "serdar.uysal@arcelik.com",
-              "TaxOffice": "",
-              "TaxId": "",
-              "Tckn": "",
-              "Address": "Kavakpınar Mah Apdi İpekçi Cad No:5",
-              "Neighborhood": "KAVAKPINAR",
-              "District": "PENDİK",
-              "City": "İSTANBUL",
-              "Urgent": "0",
-              "ContactPerson": "Deneme",
-              "ContactPhone": "2167865438",
-              "PreferredServiceShop": "NULL",
-              "DeliveryDate": "13.02.2018 08:54:00",
-              "ExternalOrderId": "",
-              "InvoiceAcceptPhone": "5373579059",
-              "InvoiceAcceptName": "Serdar",
-              "InvoiceAcceptSurname": "Uysal",
-              "ProductOrderDetail": [
-              {
-              "ConsignmentId":"1",
-              "MainSourceOrderProcessId": "DMS1",
-              "SourceOrderId": "DMS1",
-              "PK": "",
-              "R_Counter": "",
-              "SS_R_Counter": "",
-              "MainSourceOrderProcessStatus": "Approve",
-              "WareHouseCode": "12457",
-              "WareHouseType": "1",
-              "WareHouseAddress": "Depo Adresi",
-              "WareHouseNeighborhood": "BATI",
-              "WareHouseDistrict": "PENDİK",
-              "WareHouseCity": "İSTANBUL",
-              "ProductCode": "6211101000",
-              "Product": "ARY-5500 E ÇAMAŞIR MAK.(Y-326) ÇİFT",
-              "OperationType": "Montaj",
-              "ProductReturnCheck": "0",
-              "ExtraWarrantyType": "1",
-              "ProductExposeCheck": "0",
-              "SourceOrderStatus": "Approve",
-              "ProductBarcode": "",
-              "DetailNote": "Test satır 1",
-              "ParoId": "",
-              "InvoiceNr": "AAFF111SFFEWQ",
-              "InvoiceDate": "13.02.2018 08:54:00",
-              "MaliId": "",
-              "NaceId": "",
-              "SectorId": "",
-              "CrmKey": ""
-              }
-              ]
-             }
-           }
-           
-           $http(req).success(function(){
-             alert("success");
-            })
-             .error(function(){
-               alert("fail");
-             });
-
-/*
-          $http({method: 'GET', url: '/postman'}).
-          success(function(data, status) { 
-
-            var url = "https://thworkorder.azurewebsites.net/postman";
-            $window.location = url; 
-            alert(data);
-          }).
-          error(function(data, status) {
-              alert("fail");
-              $scope.dataset = data || "Request failed "; 
-          });*/
+          $http({
+            method: "GET", 
+            url: 'https://thworkorderfapp.azurewebsites.net/api/createworkorder',
+            params: {
+              name:$scope.name_id,
+              surname:$scope.surname_id,
+              phone:$scope.phone_id,
+              no:"Test",
+              product:$scope.singleSelect,
+              workorder:$scope.workorderSelect,
+              customer:"Test",
+              point:$scope.provinceSelect,
+              address:$scope.citySelect,
+              status:"Active",
+              service:"Test",
+              DeliveryDate:"2018-02-10",
+              AppointmentDate:"2018-02-15"
+            }          
+          }) 
+          .then(function(response){ 
+          });
         } 
 
 
