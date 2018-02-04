d359b01be6c03eff5044e44e4ed5abe3002d7183
Zeynep Öner <26029554@arcelik.com>
2018-02-04 21:49:38 +0300

    postman

--- client/lib/workorder.js
+++ client/lib/workorder.js
@@ -1,5 +1,5 @@
     app = angular.module('App', [])      
-    app.controller('Controller', ['$scope','$http','$window','ngRoute','ui.bootstrap','dialogs', function ($scope, $http, $window) {
+    app.controller('Controller', ['$scope','$http','$window','ngRoute','ui.bootstrap','dialogs', function ($scope, $http, $window,$ngRoute,$dialogs) {
         $scope.create = true;  
         $scope.query = false;      
         //connecting to azure db, getting required records from specified table and displaying them in selection list
