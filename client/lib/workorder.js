71427251a3514483c2c5be13ba5c9e09b4e87f97
Zeynep Öner <26029554@arcelik.com>
2018-02-05 22:33:59 +0300

    postman

--- client/lib/workorder.js
+++ client/lib/workorder.js
@@ -131,9 +131,9 @@ angular.module('App', ['ui.bootstrap','dialogs.main']);
         $scope.create_workorder = function () 
         {       
 
-          var dlg = dialogs.confirm("Lütfen Onaylayınız!","Aşağıda belirtilen bilgiler ile iş emri oluşturma talebinizi gerçekleştirmeyi onaylıyor musunuz?/nMüşteri adı = "+$scope.name_id+"/nMüşteri soyadı = "
-          +$scope.surname_id+"/nMüşteri telefon numarası = "+$scope.phone_id+"/nSeçilen ürün = "+$scope.singleSelect+"/nSeçilen iş emri türü = "
-          +$scope.workorderSelect+"/nMüşteri adresi = " + $scope.provinceSelect + " " +$scope.citySelect);
+          var dlg = dialogs.confirm("Lütfen Onaylayınız!","Aşağıda belirtilen bilgiler ile iş emri oluşturma talebinizi gerçekleştirmeyi onaylıyor musunuz?/nMüşteri adı = "+$scope.name_id+"\nMüşteri soyadı = "
+          +$scope.surname_id+"\nMüşteri telefon numarası = "+$scope.phone_id+"\nSeçilen ürün = "+$scope.singleSelect+"\nSeçilen iş emri türü = "
+          +$scope.workorderSelect+"\nMüşteri adresi = " + $scope.provinceSelect + " " +$scope.citySelect);
 
 					dlg.result.then(function(btn){
               $http({
