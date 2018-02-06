f51ceaae789788e6d2786e07a02e35c87e71252f
Zeynep Öner <26029554@arcelik.com>
2018-02-06 15:33:51 +0300

    postman

--- client/lib/workorder.js
+++ client/lib/workorder.js
@@ -162,23 +162,87 @@ angular.module('App', ['ui.bootstrap','dialogs.main']);
                 $scope.files = [];                
                 $scope.files.push("body.json");   
                 $scope.jsonData = {  
-                  name: "Jignesh Trivedi",  
-                  comments: "Multiple upload files"  
+                    "PK": "",
+                    "MainSourceApplicationProcces": "DMS",
+                    "SourceApplication": "DMS",
+                    "MainSourceOrderProccesId": "DMS1",
+                    "SourceOrderId": "DMS1",
+                    "MainSourceProccesStatus": "Approve",
+                    "SourceStatus": "Approve",
+                    "DealerCode": "342122",
+                    "AsistDealerCode": "342122",
+                    "AsistBranchDealerCode": "342122",
+                    "Note": "10",
+                    "Name": "Serdar",
+                    "Surname": "Uysal",
+                    "Phone1": "5373579059",
+                    "Phone2": "2163964187",
+                    "Phone3": "",
+                    "Email": "serdar.uysal@arcelik.com",
+                    "TaxOffice": "",
+                    "TaxId": "",
+                    "Tckn": "",
+                    "Address": "Kavakpınar Mah Apdi İpekçi Cad No:5",
+                    "Neighborhood": "KAVAKPINAR",
+                    "District": "PENDİK",
+                    "City": "İSTANBUL",
+                    "Urgent": "0",
+                    "ContactPerson": "Deneme",
+                    "ContactPhone": "2167865438",
+                    "PreferredServiceShop": "NULL",
+                    "DeliveryDate": "13.02.2018 08:54:00",
+                    "ExternalOrderId": "",
+                    "InvoiceAcceptPhone": "5373579059",
+                    "InvoiceAcceptName": "Serdar",
+                    "InvoiceAcceptSurname": "Uysal",
+                    "ProductOrderDetail": [
+                                                {
+                                                "ConsignmentId":"1",
+                                                "MainSourceOrderProcessId": "DMS1",
+                                                "SourceOrderId": "DMS1",
+                                                "PK": "",
+                                                "R_Counter": "",
+                                                "SS_R_Counter": "",
+                                                "MainSourceOrderProcessStatus": "Approve",
+                                                "WareHouseCode": "12457",
+                                                "WareHouseType": "1",
+                                                "WareHouseAddress": "Depo Adresi",
+                                                "WareHouseNeighborhood": "BATI",
+                                                "WareHouseDistrict": "PENDİK",
+                                                "WareHouseCity": "İSTANBUL",
+                                                "ProductCode": "6211101000",
+                                                "Product": "ARY-5500 E ÇAMAŞIR MAK.(Y-326) ÇİFT",
+                                                "OperationType": "Montaj",
+                                                "ProductReturnCheck": "0",
+                                                "ExtraWarrantyType": "1",
+                                                "ProductExposeCheck": "0",
+                                                "SourceOrderStatus": "Approve",
+                                                "ProductBarcode": "",
+                                                "DetailNote": "Test satır 1",
+                                                "ParoId": "",
+                                                "InvoiceNr": "AAFF111SFFEWQ",
+                                                "InvoiceDate": "13.02.2018 08:54:00",
+                                                "MaliId": "",
+                                                "NaceId": "",
+                                                "SectorId": "",
+                                                "CrmKey": ""
+                                                }
+                                          ]
                 };                
                 $scope.postData = angular.toJson($scope.jsonData, true);                
 
                   $http({
                     url: 'https://yetkiliservis-test.arcelik.com/wsaftersales/ServicePaperService.svc/ProductOrderOperationService',
                     method: "POST",
-                    transformRequest: function (data) {  
+                /*    transformRequest: function (data) {  
                         var formData = new FormData();  
                         formData.append("model", angular.toJson(data.model));  
                         for (var i = 0; i < data.files.length; i++) {  
                             formData.append("file" + i, data.files[i]);  
                         }  
                         return formData;  
-                    },  
-                    data: { model: $scope.postData, files: $scope.files },
+                    },  */
+                    data: { model: $scope.postData },
                     headers: {'Content-Type': 'application/json',
                               'SessionToken': '4737B2FC-DE66-4741-B7A8-07646813D890',
                               'cache-control': 'no-cache',
@@ -189,9 +253,6 @@ angular.module('App', ['ui.bootstrap','dialogs.main']);
                               //'Access-Control-Allow-Headers':'Accept, Access-Control-Allow-Headers, Access-Control-Allow-Credentials, Access-Control-Allow-Methods, Access-Control-Allow-Origin, servicetype, cache-control, SessionToken, Content-Type'
                              }
                   }).then(function (data, status, headers, config) {
-
-                        alert(status);
-                        alert(headers);
                   });
 					},function(btn){
 					    alert('İşlem Tamamlanamadı.');
