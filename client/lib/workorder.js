20976686d4c6193e7337f3621f3910c40edf9d4b
Zeynep Öner <26029554@arcelik.com>
2018-02-06 22:39:26 +0300

    postman

--- client/lib/workorder.js
+++ client/lib/workorder.js
@@ -248,18 +248,11 @@
                               'servicetype': 'INTHEBOX1'
                              }
                   }).then(function (response) {
+            
+                   // $scope.json = angular.toJson(response.data);
 
-                    
-                    $scope.json = angular.toJson(response.data);
-                    $scope.json_stringify = JSON.stringify(response.data); 
-
-
-                    alert(response.data)
-                    alert($scope.json)
-                    
-
-                    
-
+                    alert(response.header)
+                    //alert($scope.json)
   
                   });
 					},function(btn){
