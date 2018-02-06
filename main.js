efce893036c4972908f99d43e6bdacf7731c55f8
Zeynep Öner <26029554@arcelik.com>
2018-02-06 20:52:28 +0300

    postman

--- main.js
+++ main.js
@@ -10,19 +10,6 @@ var express = require('express'),
 var cors = require('cors');
 app.options('*', cors());
 app.use(cors());
-app.use(enableCors);
-
-function enableCors(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, SessionToken, cache-control, servicetype ');
-
-    if('OPTIONS' == req.method) {
-        res.send(200);
-    } else {
-        next();
-    }
-}
 
 
 var logFmt = require("logfmt");
