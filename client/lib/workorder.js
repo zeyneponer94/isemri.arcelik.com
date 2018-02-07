a4b9ec390efd94d0555fdf8f4008a2446da11c28
Zeynep Öner <26029554@arcelik.com>
2018-02-07 10:01:52 +0300

    postman

--- client/lib/workorder.js
+++ client/lib/workorder.js
@@ -144,6 +144,91 @@
 
 					dlg.result.then(function(btn){
 
+            $scope.jsonData = [{
+                        "PK": "3",
+                        "MainSourceApplicationProcces": "InnTheBox",
+                        "SourceApplication": "InnTheBox",
+                        "MainSourceOrderProccesId": "InnTheBox_1",
+                        "SourceOrderId": "InnTheBox_1",
+                        "MainSourceProccesStatus": "Approve",
+                        "SourceStatus": "Approve",
+                        "DealerCode": "342122",
+                        "AsistDealerCode": "342122",
+                        "AsistBranchDealerCode": "342122",
+                        "Note": "10",
+                        "Name": $scope.name_id,
+                        "Surname": $scope.surname_id,
+                        "Phone1": $scope.phone_id,
+                        "Phone2": "2163964187",
+                        "Phone3": "",
+                        "Email": $scope.email_id,
+                        "TaxOffice": "",
+                        "TaxId": "",
+                        "Tckn": "",
+                        "Address": "Kavakpınar Mah Apdi İpekçi Cad No:5",
+                        "Neighborhood": "KAVAKPINAR",
+                        "District": $scope.citySelect,
+                        "City": $scope.provinceSelect,
+                        "Urgent": "0",
+                        "ContactPerson": "Deneme",
+                        "ContactPhone": "2167865438",
+                        "PreferredServiceShop": "NULL",
+                        "DeliveryDate": "13.02.2018 08:54:00",
+                        "ExternalOrderId": "",
+                        "InvoiceAcceptPhone": $scope.phone_id,
+                        "InvoiceAcceptName": $scope.name_id,
+                        "InvoiceAcceptSurname": $scope.surname_id,
+                      "ProductOrderDetail": [
+                                      {
+                                              "ConsignmentId":"1",
+                                              "MainSourceOrderProcessId": "InnTheBox_1",
+                                              "SourceOrderId": "InnTheBox_1",
+                                              "PK": "",
+                                              "R_Counter": "",
+                                              "SS_R_Counter": "",
+                                              "MainSourceOrderProcessStatus": "Approve",
+                                              "WareHouseCode": "12457",
+                                              "WareHouseType": "1",
+                                              "WareHouseAddress": "Depo Adresi",
+                                              "WareHouseNeighborhood": "BATI",
+                                              "WareHouseDistrict": $scope.citySelect,
+                                              "WareHouseCity": $scope.provinceSelect,
+                                              "ProductCode": "6211101000",
+                                              "Product": $scope.singleSelect,
+                                              "OperationType": $scope.workorderSelect,
+                                              "ProductReturnCheck": "0",
+                                              "ExtraWarrantyType": "1",
+                                              "ProductExposeCheck": "0",
+                                              "SourceOrderStatus": "Approve",
+                                              "ProductBarcode": "",
+                                              "DetailNote": "Test satır 1",
+                                              "ParoId": "",
+                                              "InvoiceNr": "AAFF111SFFEWQ",
+                                              "InvoiceDate": "13.02.2018 08:54:00",
+                                              "MaliId": "",
+                                              "NaceId": "",
+                                              "SectorId": "",
+                                              "CrmKey": ""
+                                      }
+                                  ]
+                }];
+             
+        
+             $scope.postData = angular.toJson($scope.jsonData, true);                
+
+              $http({
+                url: 'https://yetkiliservis-test.arcelik.com/wsaftersales/ServicePaperService.svc/ProductOrderOperationService',
+                method: "POST",
+                data: $scope.postData ,
+                headers: {'Content-Type': 'application/json',
+                          'SessionToken': '548ABC6A-50FD-43C1-89D0-48B2FF59CFD6',
+                          'cache-control': 'no-cache',
+                          'servicetype': 'INTHEBOX1'
+                         }
+              }).then(function (response) {
+                  $scope.result = response.data;
+              });
+
               $http({
                 method: "GET", 
                 url: 'https://thworkorderfapp.azurewebsites.net/api/createworkorder',
@@ -157,7 +242,7 @@
                   customer:"Test",
                   point:$scope.provinceSelect,
                   address:$scope.citySelect,
-                  status:"Active",
+                  status: $scope.result.ConsignmentWorkOrderStatus,
                   service:"Test",
                   DeliveryDate:"2018-02-10",
                   AppointmentDate:"2018-02-15"
@@ -166,106 +251,6 @@
               .then(function(response){             
               });
 
-                $scope.files = [];                
-                $scope.files.push("body.json");   
-                $scope.jsonData = [{
-                            "PK": "3",
-                            "MainSourceApplicationProcces": "DMS",
-                            "SourceApplication": "DMS",
-                            "MainSourceOrderProccesId": "DMS1",
-                            "SourceOrderId": "DMS1",
-                            "MainSourceProccesStatus": "Approve",
-                            "SourceStatus": "Approve",
-                            "DealerCode": "342122",
-                            "AsistDealerCode": "342122",
-                            "AsistBranchDealerCode": "342122",
-                            "Note": "10",
-                            "Name": "Serdar",
-                            "Surname": "Uysal",
-                            "Phone1": "5373579059",
-                            "Phone2": "2163964187",
-                            "Phone3": "",
-                            "Email": "serdar.uysal@arcelik.com",
-                            "TaxOffice": "",
-                            "TaxId": "",
-                            "Tckn": "",
-                            "Address": "Kavakpınar Mah Apdi İpekçi Cad No:5",
-                            "Neighborhood": "KAVAKPINAR",
-                            "District": "PENDİK",
-                            "City": "İSTANBUL",
-                            "Urgent": "0",
-                            "ContactPerson": "Deneme",
-                            "ContactPhone": "2167865438",
-                            "PreferredServiceShop": "NULL",
-                            "DeliveryDate": "13.02.2018 08:54:00",
-                            "ExternalOrderId": "",
-                            "InvoiceAcceptPhone": "5373579059",
-                            "InvoiceAcceptName": "Serdar",
-                            "InvoiceAcceptSurname": "Uysal",
-                          "ProductOrderDetail": [
-                                          {
-                                                  "ConsignmentId":"1",
-                                                  "MainSourceOrderProcessId": "DMS1",
-                                                  "SourceOrderId": "DMS1",
-                                                  "PK": "",
-                                                  "R_Counter": "",
-                                                  "SS_R_Counter": "",
-                                                  "MainSourceOrderProcessStatus": "Approve",
-                                                  "WareHouseCode": "12457",
-                                                  "WareHouseType": "1",
-                                                  "WareHouseAddress": "Depo Adresi",
-                                                  "WareHouseNeighborhood": "BATI",
-                                                  "WareHouseDistrict": "PENDİK",
-                                                  "WareHouseCity": "İSTANBUL",
-                                                  "ProductCode": "6211101000",
-                                                  "Product": "ARY-5500 E ÇAMAŞIR MAK.(Y-326) ÇİFT",
-                                                  "OperationType": "Montaj",
-                                                  "ProductReturnCheck": "0",
-                                                  "ExtraWarrantyType": "1",
-                                                  "ProductExposeCheck": "0",
-                                                  "SourceOrderStatus": "Approve",
-                                                  "ProductBarcode": "",
-                                                  "DetailNote": "Test satır 1",
-                                                  "ParoId": "",
-                                                  "InvoiceNr": "AAFF111SFFEWQ",
-                                                  "InvoiceDate": "13.02.2018 08:54:00",
-                                                  "MaliId": "",
-                                                  "NaceId": "",
-                                                  "SectorId": "",
-                                                  "CrmKey": ""
-                                          }
-                                      ]
-                    }];
-                 
-            
-                $scope.postData = angular.toJson($scope.jsonData, true);                
-
-                  $http({
-                    url: 'https://yetkiliservis-test.arcelik.com/wsaftersales/ServicePaperService.svc/ProductOrderOperationService',
-                    method: "POST",
-                    data: $scope.postData ,
-                    headers: {'Content-Type': 'application/json',
-                              'SessionToken': '548ABC6A-50FD-43C1-89D0-48B2FF59CFD6',
-                              'cache-control': 'no-cache',
-                              'servicetype': 'INTHEBOX1'
-                             }
-                  }).then(function (response) {
-                      $scope.result = response.data;
-                   // obj = JSON.parse(response);
-                    //alert(obj.ConsignmentWorkOrderStatus)
-                   
-                    //var headers = response.headers();
-                    //alert(headers['content-type']);
-
-            /*        if(headers['content-type']==" application/json; charset=utf-8")
-                    {
-                      //alert(response.data);           
-                      obj = JSON.parse(response.data);
-                      alert(obj.ErrorCode);        
-                      alert(JSON.stringify(response.data).ErrorCode);   
-                    }*/
-
-                  });
 					},function(btn){
 					    alert('İşlem Tamamlanamadı.');
 					});
