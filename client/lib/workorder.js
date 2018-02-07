8b539a20656147e465fb90ced56cf73c314a1c62
Zeynep Öner <26029554@arcelik.com>
2018-02-07 11:41:19 +0300

    postman

--- client/lib/workorder.js
+++ client/lib/workorder.js
@@ -46,8 +46,8 @@
         for (var i = 0; i < bars.length; i++) {
           bars[i].addEventListener("click", function() {
             var current = document.getElementsByClassName("active");
-            current[0].className = current[0].className.replace("active", "");
-            this.className += "active";
+            current[0].className = current[0].className.replace(" active", "");
+            this.className += " active";
           });
         }   
 
