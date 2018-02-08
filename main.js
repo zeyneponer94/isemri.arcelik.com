1df4c19d157812146f28cec0eb3b5d9e6875bd8a
Zeynep Öner <26029554@arcelik.com>
2018-02-08 14:09:01 +0300

    postman

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function enableCors(req, res, next) {
         res.header('Access-Control-Allow-Origin: https://yetkiliservis-test.arcelik.com/wsaftersales/ServicePaperService.svc/ProductOrderOperationService, https://thworkorder.azurewebsites.net'); 
         res.header("Access-Control-Allow-Credentials: true");
         res.header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-        res.header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Access-Control-Allow-Origin,Access-Control-Allow-Methods, Access-Control-Max-Age,Origin, Content-Type, X-Auth-Token , Authorization,SessionToken,Cache-Control,servicetype');
+        res.header('Access-Control-Allow-Headers: Accept,Access-Control-Allow-Headers,Access-Control-Allow-Origin,Access-Control-Allow-Methods, Access-Control-Max-Age,Origin, Content-Type, X-Auth-Token , Authorization,SessionToken,Cache-Control,servicetype');
         next();
 
 }
