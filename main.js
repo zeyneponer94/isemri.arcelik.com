767a4d8197cdf87874e3a28e3398720e461e2c4d
Zeynep Öner <26029554@arcelik.com>
2018-02-08 14:11:28 +0300

    postman

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function enableCors(req, res, next) {
         res.header("Access-Control-Allow-Credentials: true");
         res.header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
         res.header('Access-Control-Allow-Headers: Accept,Access-Control-Allow-Headers,Access-Control-Allow-Origin,Access-Control-Allow-Methods, Access-Control-Max-Age,Origin, Content-Type, X-Auth-Token , Authorization,SessionToken,Cache-Control,servicetype');
-        next();
+        res.send(200);
 
 }
 
