6e58de275007463b5756b22e7ae21e762baa4074
Zeynep Öner <26029554@arcelik.com>
2018-02-08 22:47:08 +0300

    postman

--- client/lib/login.js
+++ client/lib/login.js
@@ -4,7 +4,7 @@ testApp.controller('Controller' , ['$scope','$http','$window', function ($scope,
     
     $http({method: 'GET', url: 'https://thworkorderfapp.azurewebsites.net/deneme'}).
     success(function(data, status) { 
-        alert(data.body.name);
+        alert(data.body);
     }).
     error(function(data, status) {
         alert(data);
