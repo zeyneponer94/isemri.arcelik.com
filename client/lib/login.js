c2291906fda233ef31d85ac03ca0f2121ddd104b
Zeynep Öner <26029554@arcelik.com>
2018-02-11 01:04:23 +0300

    postman

--- client/lib/login.js
+++ client/lib/login.js
@@ -75,7 +75,7 @@ testApp.controller('Controller' , ['$scope','$http','$window', function ($scope,
         xhr.setRequestHeader("SessionToken", "346719C1-5134-4F04-816E-D1095485E41A");
         xhr.setRequestHeader("Cache-Control", "no-cache");
         xhr.setRequestHeader("servicetype", "INTHEBOX1");    
-        xhr.send(data);  
+        xhr.send();  
         xhr.onreadystatechange = function () {
             if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
                 alert(xhr.responseText);
