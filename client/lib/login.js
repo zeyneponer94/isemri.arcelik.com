cc3ce6b8e96b6322ac653fc2ea883cdf939cfaba
Zeynep Öner <26029554@arcelik.com>
2018-02-10 23:51:39 +0300

    postman

--- client/lib/login.js
+++ client/lib/login.js
@@ -78,7 +78,6 @@ testApp.controller('Controller' , ['$scope','$http','$window', function ($scope,
         xhr.send(data);  
         xhr.onreadystatechange = function () {
             if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-                alert(resolve(xhr.response));                
                 alert(JSON.parse(xhr.responseText));
                 alert(xhr.response);
             }
