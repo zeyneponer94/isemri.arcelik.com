315b1c0a09d220501ba86004caa67d953423665e
Zeynep Öner <26029554@arcelik.com>
2018-03-23 11:54:47 +0300

    wsaftersales

--- client/lib/login.js
+++ client/lib/login.js
@@ -1,7 +1,72 @@
-var testApp = angular.module("App", []);
-testApp.controller('Controller' , ['$scope','$http','$window', function ($scope, $http, $window) {
+var testApp = angular.module("App", ['BotDetectCaptcha']);
+
+testApp.config(function(captchaSettingsProvider) {
+    captchaSettingsProvider.setSettings({
+      captchaEndpoint: '/bdc4-simple-api-angularjs-captcha-example/botdetectcaptcha'
+    });
+  });
+
+testApp.controller('Controller' , ['$scope','$http','$window', function ($scope, $http, $window, Captcha) {
+
+
+
+     // captcha validation messages
+  $scope.successMessages = '';
+  $scope.errorMessages = '';
+  
+  // basic captcha url
+  var basicUrl = '/bdc4-simple-api-angularjs-captcha-example/basic-captcha';
+  
+  $scope.validate = function(valid) {
+
+    if (!valid) {
+      return;
+    }
+    
+    // after UI form validation passed, 
+    // we will need to validate captcha at server-side once before we save form data in database, etc.
+    
+    // create new BotDetect Captcha instance
+    var captcha = new Captcha();
+    
+    // captcha id for validating captcha at server-side
+    var captchaId = captcha.captchaId;
+    
+    // captcha code input value for validating captcha at server-side
+    var captchaCode = $scope.captchaCode;
+
+    var postData = {
+      captchaId: captchaId,
+      captchaCode: captchaCode
+    };
+    
+    $http({
+      method: 'POST',
+      url: basicUrl,
+      data: JSON.stringify(postData)
+    })
+      .then(function(response) {
+        if (response.data.success) {
+          // captcha validation passed at server-side
+          $scope.successMessages = 'CAPTCHA validation passed.';
+          $scope.errorMessages = null;
+        } else {
+          // captcha validation falied at server-side
+          $scope.errorMessages = 'CAPTCHA validation falied.';
+          $scope.successMessages = null;
+        }
+        
+        // always reload captcha image after validating captcha at server-side 
+        // in order to update new captcha code for current captcha id
+        captcha.reloadImage();
+      }, function(error) {
+        console.log(error.data);
+      });
+  };
+   
 
     $scope.submit = function (model) {    
+        
         $http({
             method: "GET",        
             url: 'https://thworkorderfapp.azurewebsites.net/api/systemlogin',             
@@ -9,8 +74,9 @@ testApp.controller('Controller' , ['$scope','$http','$window', function ($scope,
         }). 
         success(function(data, status) { 
             alert(data)
-            if(status == 200)
+            if(status == 200){
                 $scope.login();
+            }
         }).
         error(function(data, status) {
             alert(data);
