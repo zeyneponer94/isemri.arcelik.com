8acf0ff86db6672c50fc4a909b9eba7e5482a040
Zeynep Öner <26029554@arcelik.com>
2018-03-26 23:19:37 +0300

    captcha

--- main.js
+++ main.js
@@ -30,6 +30,7 @@ app.post('/submit',function(req,res){
     // Hitting GET request to the URL, Google will respond with success or error scenario.
     request(verificationUrl,function(error,response,body) {
       body = JSON.parse(body);
+      res.send(body);
       // Success will be true or false depending upon captcha validation.
       if(body.success !== undefined && !body.success) {
         return res.json({"responseCode" : 1,"responseDesc" : "Failed captcha verification"});
