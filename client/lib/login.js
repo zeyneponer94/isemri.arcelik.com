eb50b312f892b48d2e5e0ba3faf5ebb8ffd48f6f
Zeynep Öner <26029554@arcelik.com>
2018-03-26 16:37:08 +0300

    wsaftersales

--- client/lib/login.js
+++ client/lib/login.js
@@ -2,7 +2,7 @@ var testApp = angular.module("App", ['BotDetectCaptcha']);
 
 testApp.config(function(captchaSettingsProvider) {
     captchaSettingsProvider.setSettings({
-      captchaEndpoint: 'WEB-INF/lib/web.xml'
+      captchaEndpoint: 'WEB-INF/lib/com.captcha.botdetect.web.servlet.SimpleCaptchaServlet'
     });
   });
   
