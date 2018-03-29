7e2fd3098a740ab4c8876c50b505a216797cc8bf
Zeynep Öner <26029554@arcelik.com>
2018-03-29 09:44:34 +0300

    okta is added

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ var express = require('express'),
 var logFmt = require("logfmt");
 
 app.get('/', function(req, res) {
-    res.sendfile(__dirname + 'client/samples-js-angular/custom-login/src/app/login/login.component.html');      
+    res.sendfile('client/samples-js-angular/custom-login/src/app/login/login.component.html', {root: __dirname });       
 });
 app.get('/workorder' , function(req,res) {
     res.sendfile('views/create_workorder.html', {root: __dirname });   
