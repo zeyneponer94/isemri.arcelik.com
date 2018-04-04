c80d6a37d6b3b03cc5f331ae383fb836953aec68
Zeynep Öner <26029554@arcelik.com>
2018-04-04 15:56:39 +0300

    query_all and query_workorder are changed

--- main.js
+++ main.js
@@ -4,16 +4,39 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     errorHandler = require('express-error-handler'),
     app = express();
+    connect = require('connect'),
+    auth = require('./auth');
     Console = require('console');
-
 var logFmt = require("logfmt");
 
+/*
 app.get('/', function(req, res) {
     res.sendfile('views/login_page.html', {root: __dirname });       
 });
 app.get('/workorder' , function(req,res) {
     res.sendfile('views/create_workorder.html', {root: __dirname });   
+});*/
+
+//Lets call passport authenticate method to authenticate 
+app.get('/', auth.authenticate('saml', { failureRedirect: '/', failureFlash: true }), function(req, res) {
+    res.redirect('/');
 });
+
+//POST Methods, redirect to home successful login
+app.post('/workorder', auth.authenticate('saml', { failureRedirect: '/', failureFlash: true }), function(req, res) {
+    res.redirect('/workorder');
+});
+
+//Get Methods
+app.get('/', auth.protected, function(req, res) {
+    res.sendfile('views/login_page.html', {root: __dirname });       
+});
+
+app.get('/workorder', auth.protected, function(req, res) {
+    res.sendfile('views/create_workorder.html', {root: __dirname });   
+});
+
+
 
 app.post('/submit',function(req,res){
     //res.send(req.body['g-recaptcha-response']);    
@@ -45,6 +68,14 @@ app.get('/register' , function(req,res) {
 app.set('port', process.env.PORT || 1337);
 app.use(express.static(__dirname + '/client')); 
 app.use(errorHandler());
+app.use(express.logger());
+app.use(connect.compress());
+app.use(express.cookieParser());
+app.use(express.bodyParser());
+app.use(express.session({ secret: "won't tell because it's secret"  }));
+app.use(auth.initialize());
+app.use(auth.session());
+
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
