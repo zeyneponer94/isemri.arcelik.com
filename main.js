79dfd5d205fd3091b91c874182d1a47bd1c198e5
Zeynep Öner <26029554@arcelik.com>
2018-04-05 13:29:04 +0300

    query_all and query_workorder are changed

--- main.js
+++ main.js
@@ -30,31 +30,6 @@ app.get('/workorder', auth.protected, function(req, res) {
 });
 
 
-
-/*
-app.post('/submit',function(req,res){
-    //res.send(req.body['g-recaptcha-response']);    
-    // g-recaptcha-response is the key that browser will generate upon form submit.
-    // if its blank or null means user has not selected the captcha, so return the error.
-    if(req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null) {
-      return res.json({"responseCode" : 1,"responseDesc" : "Please select captcha"});
-    }
-
-    // Put your secret key here.
-    var secretKey = "6LfuDE8UAAAAAAH7G69uBc7aONOVQ4d23A24Hiu5";
-    // req.connection.remoteAddress will provide IP address of connected user.
-    var verificationUrl = "https://thworkorderfapp.azurewebsites.net/google_captcha?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
-    // Hitting GET request to the URL, Google will respond with success or error scenario.
-    request(verificationUrl,function(error,response,body) {
-      body = JSON.parse(body);
-      // Success will be true or false depending upon captcha validation.
-      if(body.success !== undefined && !body.success) {
-        return res.json({"responseCode" : 1,"responseDesc" : "Failed captcha verification"});
-      }
-      return res.json({"responseCode" : 0,"responseDesc" : "Success"});
-    });
-  });*/
-
 app.get('/register' , function(req,res) {
     res.sendfile('views/register.html', {root: __dirname });   
 });
