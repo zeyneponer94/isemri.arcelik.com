afc6cfe0500c5ced540229fa1c350c0e35aa9362
Zeynep Öner <26029554@arcelik.com>
2018-04-05 09:01:08 +0300

    query_all and query_workorder are changed

--- main.js
+++ main.js
@@ -66,10 +66,10 @@ app.use(express.logger());
 app.use(connect.compress());
 app.use(express.cookieParser());
 app.use(express.bodyParser());
-app.use(express.session({ secret: "won't tell because it's secret"  }));
 app.use(auth.initialize());
 app.use(auth.session());
-
+app.use(everyauth.middleware());
+app.use(express.session({ secret: "won't tell because it's secret"  }));
 
 var currentPort = app.listen(process.env.PORT || 3000);
 
