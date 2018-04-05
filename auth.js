addba3a699a68548e9ed43b3979a8549c1784b73
Zeynep Öner <26029554@arcelik.com>
2018-04-05 15:57:26 +0300

    query_all and query_workorder are changed

--- auth.js
+++ auth.js
@@ -1,6 +1,7 @@
 var passport = require('passport');
 var SamlStrategy = require('passport-saml').Strategy;
-var config = require('./config.json')[process.env.NODE_ENV];
+var config = require('./config.json');
+//[process.env.NODE_ENV || 'dev'];
 
 var users = [];
 
