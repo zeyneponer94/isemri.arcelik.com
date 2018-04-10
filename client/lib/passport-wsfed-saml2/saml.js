55fc2ddd56440cc43092a1c4655bf119513c7de9
Zeynep Öner <26029554@arcelik.com>
2018-04-10 16:26:41 +0300

    comment

---
+++ client/lib/passport-wsfed-saml2/saml.js
@@ -0,0 +1,123 @@
+// credits to: https://github.com/bergie/passport-saml
+
+var xml2js = require('xml2js');
+var xmlCrypto = require('xml-crypto');
+var crypto = require('crypto');
+var xmldom = require('xmldom');
+var querystring = require('querystring');
+
+var SAML = function (options) {
+  this.options = options;
+
+  if (!options.cert && !options.thumbprint) {
+    throw new Error('You should set either a base64 encoded certificate or the thumbprint of the certificate');
+  }
+};
+
+SAML.prototype.certToPEM = function (cert) {
+  cert = cert.match(/.{1,64}/g).join('\n');
+  cert = "-----BEGIN CERTIFICATE-----\n" + cert;
+  cert = cert + "\n-----END CERTIFICATE-----\n";
+  return cert;
+};
+
+SAML.prototype.validateSignature = function (xml, cert, thumbprint) {
+  var self = this;
+  var doc = new xmldom.DOMParser().parseFromString(xml);
+  var signature = xmlCrypto.xpath.SelectNodes(doc, "/*/*[local-name(.)='Signature' and namespace-uri(.)='http://www.w3.org/2000/09/xmldsig#']")[0];
+  var sig = new xmlCrypto.SignedXml();
+  sig.keyInfoProvider = {
+    getKeyInfo: function (key) {
+      return "<X509Data></X509Data>"
+    },
+    getKey: function (keyInfo) {
+      if (thumbprint)  {
+        var embeddedSignature = keyInfo[0].getElementsByTagName("X509Certificate");
+        if (embeddedSignature.length > 0) {
+          var base64cer = embeddedSignature[0].firstChild.toString();
+          var shasum = crypto.createHash('sha1');
+          var der = new Buffer(base64cer, 'base64').toString('binary')
+          shasum.update(der);
+          self.calculatedThumbprint = shasum.digest('hex');
+    
+          return self.certToPEM(base64cer);
+        }
+      }
+      
+      return self.certToPEM(cert);
+    }
+  };
+  sig.loadSignature(signature.toString());
+  var valid = sig.checkSignature(xml);
+
+  if (cert) {
+    return valid;
+  }
+
+  if (thumbprint) {
+    return valid && this.calculatedThumbprint.toUpperCase() === thumbprint.toUpperCase();
+  }
+};
+
+SAML.prototype.validateExpiration = function (samlAssertion) {
+  var notBefore = new Date(samlAssertion.Conditions['@'].NotBefore);
+  notBefore = notBefore.setMinutes(notBefore.getMinutes() - 10); // 10 minutes clock skew
+  
+  var notOnOrAfter = new Date(samlAssertion.Conditions['@'].NotOnOrAfter);
+  notOnOrAfter = notOnOrAfter.setMinutes(notOnOrAfter.getMinutes() + 10); // 10 minutes clock skew
+
+  var now = new Date();
+
+  if (now < notBefore || now > notOnOrAfter)
+    return false;
+
+  return true;
+};
+
+SAML.prototype.validateAudience = function (samlAssertion, realm) {
+  var audience = samlAssertion.Conditions.AudienceRestriction.Audience;
+  return audience === realm;
+};
+
+SAML.prototype.validateResponse = function (samlAssertionString, callback) {
+  var self = this;
+  
+  // Verify signature
+  if (!self.validateSignature(samlAssertionString, self.options.cert, self.options.thumbprint)) {
+    return callback(new Error('Invalid signature'), null);
+  }
+
+  var parser = new xml2js.Parser();
+  parser.parseString(samlAssertionString, function (err, samlAssertion) {
+    
+    if (!self.validateExpiration(samlAssertion)) {
+      return callback(new Error('Token has expired.'), null);
+    }
+
+    if (!self.validateAudience(samlAssertion, self.options.realm)) {
+      return callback(new Error('Audience is invalid. Expected: ' + self.options.realm), null);
+    }
+
+    profile = {};
+    profile.issuer = samlAssertion.Issuer;
+
+    var attributeStatement = samlAssertion.AttributeStatement;
+    var attributes = attributeStatement.Attribute;
+    attributes.forEach(function (attribute) {
+      var value = attribute.AttributeValue;
+      if (typeof value === 'string') {
+        profile[attribute['@'].Name] = value;
+        return;
+      }
+      profile[attribute['@'].Name] = value['#'];
+    });
+
+    if (!profile.email && profile['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress']) {
+      profile.email = profile['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress'];
+    }
+
+    callback(null, profile);
+  });
+};
+
+exports.SAML = SAML;
