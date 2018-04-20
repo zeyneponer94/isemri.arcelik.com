d3ca03e51edbe0d7a2e8b75d6d5d6a09bcba5318
Zeynep Öner <26029554@arcelik.com>
2018-04-20 13:05:11 +0300

    comment

--- main.js
+++ main.js
@@ -16,30 +16,27 @@ var cookieSession = require('cookie-session');
 
 //Lets call passport authenticate method to authenticate 
 app.get('/login', auth.authenticate('saml', { failureRedirect: '/', failureFlash: true }), function(req, res) {
+    res.send("login");
     res.redirect('/');
 });
 
 //POST Methods, redirect to home successful login
 app.post('/login/callback', auth.authenticate('saml', { failureRedirect: '/', failureFlash: true }), function(req, res) {
-    res.redirect('/');
+    res.send("login/callback");    
+    res.redirect('/workorder');
 });
 
 //Get Methods
 app.get('/', auth.protected, function(req, res) {
+    res.send("login");    
     res.sendfile('views/login_page.html', {root: __dirname });       
 });
-/*
+
 app.get('/workorder', auth.protected, function(req, res) {
+    res.send("login/callback");        
     res.sendfile('views/create_workorder.html', {root: __dirname });   
 });
-*/
-/*
-app.get('/' , function(req,res) {
-    res.sendfile('views/login_page.html', {root: __dirname });   
-});
-app.get('/workorder' , function(req,res) {
-    res.sendfile('views/create_workorder.html', {root: __dirname });   
-});*/
+
 app.get('/register' , function(req,res) {
     res.sendfile('views/register.html', {root: __dirname });   
 });
