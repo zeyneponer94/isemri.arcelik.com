b5544b5a69ea8cc6c5437b83e3b92390f69a0334
Zeynep Öner <26029554@arcelik.com>
2018-04-21 16:04:22 +0300

    comment

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ app.get('/', function(req, res) {
 });
 
 app.get('/workorder', function(req, res) {
-    res.send(req.header);
+    res.send(req.headers);
     res.sendfile('views/create_workorder.html', {root: __dirname });  
 
 });
