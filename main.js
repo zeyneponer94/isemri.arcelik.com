60d455b476a396d1da6a11025c0a3ace3fefdab6
Zeynep Öner <26029554@arcelik.com>
2018-04-21 14:51:02 +0300

    comment

--- main.js
+++ main.js
@@ -50,8 +50,8 @@ app.use(connect.compress());
 app.use(express.session({ secret: "won't tell because it's secret"  }));
 app.use(express.logger());
 app.use(errorHandler());
-app.use(auth.initialize());
-app.use(auth.session());
+//app.use(auth.initialize());
+//app.use(auth.session());
 app.use(express.static(__dirname + '/client')); 
 var currentPort = app.listen(process.env.PORT || 3000);
 
