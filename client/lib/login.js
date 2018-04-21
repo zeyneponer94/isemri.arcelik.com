7fd496553b0a5e12397783b53e91361f5781459c
Zeynep Öner <26029554@arcelik.com>
2018-04-21 23:37:45 +0300

    comment

--- client/lib/login.js
+++ client/lib/login.js
@@ -1,6 +1,19 @@
 
 var testApp = angular.module("App", ['ui.bootstrap','dialogs.main','ngRoute','ngSanitize','ui.mask']);
 
+testApp.directive('ngEnter', function () { 
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if (event.which === 13) {
+                scope.$apply(function () {
+                    scope.$eval(attrs.ngEnter);
+                });
+                event.preventDefault();
+            }
+        });
+   };
+})
+  
 testApp.service('sharedProperties', function () {
     var GuId = '';
     return {
