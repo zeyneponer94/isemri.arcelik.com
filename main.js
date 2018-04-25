8ac91d855935c1b6a45709f988989e1276444fa4
Zeynep Öner <26029554@arcelik.com>
2018-04-25 11:31:32 +0300

    okta integration

--- main.js
+++ main.js
@@ -48,7 +48,6 @@ app.get('/workorder', auth.protected, function(req, res) {
     res.sendfile('views/create_workorder.html', {root: __dirname });   
 });
 
-
 /*
 //Get Methods
 app.get('/', function(req, res) {
@@ -59,11 +58,6 @@ app.get('/workorder', function(req, res) {
     res.sendfile('views/create_workorder.html', {root: __dirname });  
 
 });*/
-
-//Get Methods
-app.get('/', function(req, res) {
-    res.sendfile('views/login_page.html', {root: __dirname });       
-});
 
 //Get Methods
 app.get('/home', function(req, res) {
