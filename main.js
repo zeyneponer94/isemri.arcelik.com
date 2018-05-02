1d43cfd00b06feedad0e10728186eb398eaf8a58
Zeynep Öner <26029554@arcelik.com>
2018-05-02 13:43:11 +0300

    commit

--- main.js
+++ main.js
@@ -65,6 +65,7 @@ app.get('/', auth.protected, function(req, res) {
 });
 
 app.get('/workorder', auth.protected, function(req, res) {
+    res.send(req.session);
     res.cookie('user', '' + req.session.passport.user, { maxAge: 900000, httpOnly: false });       
     res.sendfile('views/create_workorder.html', {root: __dirname });   
 });
