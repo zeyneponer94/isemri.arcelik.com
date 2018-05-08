ac3cb7e6e9d38bae5157e6e39ddf743311f313c3
Zeynep Öner <26029554@arcelik.com>
2018-05-08 14:14:14 +0300

    commit

--- main.js
+++ main.js
@@ -30,7 +30,8 @@ app.post('/login/callback', auth.authenticate('saml', { failureRedirect: '/fail'
 
 //Get Methods
 app.get('/', auth.protected, function(req, res) {
-    res.send(req.cookies);
+    res.send(req.sessionID);
+    //res.send(req.cookies);
    // res.cookie('sessionID', '' + req.cookies.sessionID, { maxAge: 900000, httpOnly: false });               
     //res.cookie('user', '' + req.user.nameID, { maxAge: 900000, httpOnly: false });           
     res.sendfile('views/create_workorder.html', {root: __dirname });   
