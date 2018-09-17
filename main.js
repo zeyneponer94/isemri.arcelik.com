4b12c6a9900b8fd688a8ef1872db1a2d650c0007
Zeynep Öner <zeynep_oner@arcelik.com>
2018-09-17 13:12:20 +0000

    Updated main.js

--- main.js
+++ main.js
@@ -38,69 +38,18 @@ app.post('/login/callback', auth.authenticate('saml', { failureRedirect: '/fail'
     res.redirect('/');
 });
 
-/*
-app.get('/decrypt', auth.protected, function(req, res) {
 
-   
-
-});   */ 
-//Get Methods
 app.get('/', auth.protected, function(req, res) {
-/*
-    var username = req.user.username;
-    var cipher = crypto.createCipher(algorithm,password)
-    var crypted = cipher.update(username,'utf8','hex')
-    crypted += cipher.final('hex');
-
-   // res.send(crypted);
-
-    fs.writeFile("test.txt", crypted, function(err) {
-        if(err) {
-            return console.log(err);
-        }
-    });
-
-//send get request from login.js to /decrypt 
-    var decipher = crypto.createDecipher(algorithm,password)
-    var dec = decipher.update(crypted,'hex','utf8')
-    dec += decipher.final('utf8');  
-
-    console.log(dec)
-
-    fs.writeFile("test2.txt", dec, function(err) {
-        if(err) {
-            return console.log(err);
-        }
-    });
-
-*/
-
     res.cookie('sessionID', '' + req.sessionID, { maxAge: 900000, httpOnly: false }); 
     res.cookie('username', '' + req.user.username, { maxAge: 900000, httpOnly: false });    
     res.cookie('email', '' + req.user.email, { maxAge: 900000, httpOnly: false });               
     res.sendfile('views/create_workorder.html', {root: __dirname });   
 });
 
-/*
-app.get('/workorder', auth.protected, function(req, res) {
-    res.cookie('user', '' + req.user.nameID, { maxAge: 900000, httpOnly: false });       
-    res.sendfile('views/create_workorder.html', {root: __dirname });   
-});*/
-
 app.get('/fail', function(req, res) {
     res.send("Request Failed");       
 });
 
-/*
-//Get Methods
-app.get('/home', function(req, res) {
-    res.sendfile('views/home.html', {root: __dirname });       
-});
-
-app.get('/register' , function(req,res) {
-    res.sendfile('views/register.html', {root: __dirname });   
-});
-*/
 app.use(express.static(__dirname + '/client')); 
 var currentPort = app.listen(process.env.PORT || 3000);
 
